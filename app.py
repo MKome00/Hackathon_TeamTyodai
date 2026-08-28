ac85991fd650aa0d3edb6cc818076a09080d80a2
MKome00 <komei725725@gmail.com>
2026-08-28 22:52:36 +0900

    コメント追加

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template  # Flask本体を作るためのFlaskクラスと、HTMLファイルを表示するためのrender_template関数を読み込む
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
+app = Flask(__name__)  # このPythonファイルをもとにFlaskアプリを作成し、appという変数に保存する
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/")  # ブラウザでトップページ「/」にアクセスされたときに、直下のindex関数を実行するよう設定する
+def index():  # トップページにアクセスされたときに実行されるindex関数を定義する
+    return render_template("index.html")  # templatesフォルダ内のindex.htmlを読み込み、ブラウザに表示する
+
+
+if __name__ == "__main__":  # このapp.pyファイルが直接実行された場合だけ、以下の処理を実行する
+    app.run(debug=True)  # Flaskの開発用サーバーをデバッグモードで起動する
